212a9604ce06c3fa15638ac05408812be9857bb1
Michael <Michaelbran111@gmail.com>
2022-09-14 17:42:36 -0400

    added comments

--- src/Project0/Project0.java
+++ src/Project0/Project0.java
@@ -6,18 +6,17 @@ public class Project0 {
 
     public static void main(String[] args) {
 
-
         while (true) {
-
+            //counter for lower and upper case e's
             int upperCounter = 0;
             int lowerCounter = 0;
-
+            //dialog to ask for input
             String inputSentence = JOptionPane.showInputDialog(null, "Please enter a sentence.");
-
+            //exit sequence
             if("Stop".equalsIgnoreCase(inputSentence)) {
                 System.exit(0);
             }
-
+            // check for lower and upper case e's
             for(int i = 0; i < inputSentence.length(); i++){
                 if(inputSentence.charAt(i) == 'e'){
                     lowerCounter++;
@@ -26,6 +25,7 @@ public class Project0 {
                     upperCounter++;
                 }
             }
+            // message dialog with response
             JOptionPane.showMessageDialog(null, "The number of lower case e's: " +lowerCounter +" \nThe number of upper case E's: " +upperCounter);
         }
     }
