3c8be34c17149e3f1b0d8b1a53bd2037a41b1449
blue <michaelbran111@gmail.com>
2022-09-14 13:59:43 -0400

    Finished Project0

--- src/Project0/Project0.java
+++ src/Project0/Project0.java
@@ -1,4 +1,32 @@
 package Project0;
 
+import javax.swing.*;
+
 public class Project0 {
+
+    public static void main(String[] args) {
+
+
+        while (true) {
+
+            int upperCounter = 0;
+            int lowerCounter = 0;
+
+            String inputSentence = JOptionPane.showInputDialog(null, "Please enter a sentence.");
+
+            if(inputSentence.equalsIgnoreCase("Stop")) {
+                System.exit(0);
+            }
+
+            for(int i = 0; i < inputSentence.length(); i++){
+                if(inputSentence.charAt(i) == 'e'){
+                    lowerCounter++;
+                }
+                else if(inputSentence.charAt(i) == 'E'){
+                    upperCounter++;
+                }
+            }
+            JOptionPane.showMessageDialog(null, "The number of lower case e's:" + lowerCounter +  " \nThe number of upper case E's:" + upperCounter);
+        }
+    }
 }
